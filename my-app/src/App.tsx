45f7293a7f067a0bf7ac3e79d81d6929c1aad28c
Adrian <adrianbueno095@gmail.com>
2023-11-01 16:24:44 +0100

    testing api

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,10 +1,29 @@
+import { useEffect, useState } from "react";
 import "./App.scss";
 import SearchAppBar from "./components/SearchAppBar";
 
 function App() {
+  const [events, setEvents] = useState<string[]>([]);
+  const getEvents = async (): Promise<any> => {
+    await fetch("https://teclead-ventures.github.io/data/london-events.json")
+      .then((response) => response.json())
+      .then((data) => setEvents(data));
+  };
+
+  useEffect(() => {
+    getEvents();
+  }, []);
+
   return (
     <div className="App">
       <SearchAppBar />
+      <div className="Events-galllery">
+        {events.map((event, index) => (
+          <div className="event-item" key={index}>
+            <img src={event} alt="Event" />
+          </div>
+        ))}
+      </div>
       {/* <MenuAppBar /> */}
     </div>
   );
