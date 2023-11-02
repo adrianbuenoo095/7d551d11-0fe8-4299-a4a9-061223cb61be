786e6255153e6ea8848bbf83721a899a2d9a41c5
Adrian <adrianbueno095@gmail.com>
2023-11-02 14:51:09 +0100

    add a new event props

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,12 +2,28 @@ import { useEffect, useState } from "react";
 import "./App.scss";
 import SearchAppBar from "./components/SearchAppBar";
 
+interface EventProps {
+  _id: string;
+  title: string;
+  flyerFront: string;
+  startTime: string;
+  endTime: string;
+  venue: {
+    id: string;
+    name: string;
+  };
+}
+
 function App() {
-  const [events, setEvents] = useState<string[]>([]);
+  const [events, setEvents] = useState<EventProps[]>([]);
   const getEvents = async (): Promise<any> => {
-    await fetch("https://teclead-ventures.github.io/data/london-events.json")
+    return await fetch(
+      "https://teclead-ventures.github.io/data/london-events.json"
+    )
       .then((response) => response.json())
-      .then((data) => setEvents(data));
+      .then((data) => {
+        setEvents(data);
+      });
   };
 
   useEffect(() => {
@@ -18,9 +34,13 @@ function App() {
     <div className="App">
       <SearchAppBar />
       <div className="Events-galllery">
-        {events.map((event, index) => (
-          <div className="event-item" key={index}>
-            <img src={event} alt="Event" />
+        {events.map((event) => (
+          <div className="event-item" key={event._id}>
+            <div>{event.title}</div>
+            <img src={event.flyerFront} alt="Event" />
+            <div>{event.venue.name}</div>
+            <div>{event.startTime}</div>
+            <div>{event.endTime}</div>
           </div>
         ))}
       </div>
