470ab738276e1fce2a683847dfc2b3b1ceb7b34a
Adrian <adrianbueno095@gmail.com>
2023-11-10 02:01:07 +0100

    update SearchProviderProps

--- my-app/src/components/provider/SearchProvider.tsx
+++ my-app/src/components/provider/SearchProvider.tsx
@@ -2,7 +2,7 @@ import { ReactNode, useState } from "react";
 import { SearchContext } from "../context/SearchContext";
 
 export interface SearchProviderProps {
-  children?: ReactNode;
+  children: ReactNode;
 }
 const SearchProvider = ({ children }: SearchProviderProps) => {
   const [searchQuery, setSearchQuery] = useState<string>("");
