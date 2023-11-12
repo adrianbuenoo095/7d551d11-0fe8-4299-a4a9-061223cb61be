1127bdc58751934fb314d91ae51db21f7f84debd
Adrian <adrianbueno095@gmail.com>
2023-11-13 00:03:05 +0100

    add a  CartProps

--- my-app/src/components/Cart/Cart.tsx
+++ my-app/src/components/Cart/Cart.tsx
@@ -1,17 +1,18 @@
+import { EventProps } from "../Card";
 import styles from "./Cart.module.scss";
 
-// interface CartProps {
-//   cartEvents: EventProps[];
-//   addToCart: (clickedItem: EventProps) => void;
-//   removeFromCart: (id: number) => void;
-// }
+interface CartProps {
+  cartEvents: EventProps[];
+  addToCart: (clickedItem: EventProps) => void;
+  removeFromCart: (id: number) => void;
+}
 
 const Cart = () => {
   return (
     <>
       <div className={styles.cartContainer}>
         <div>Your Cart</div>
-        {/* <div>{cartEvents.length === 0 ? <p>No Events in cart.</p> : null}</div> */}
+        <div>{cartEvents.length === 0 ? <p>No Events in cart.</p> : null}</div>
       </div>
     </>
   );
